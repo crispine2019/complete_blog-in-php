c99bca2de2f65271c272815be424287d915940fd
crispine2019 <siakamacrispine@gmail.com>
2021-06-30 14:31:06 +0300

    added

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+			<p>crispine &copy; <?php echo date('Y'); ?></p>
+		</div>
+		<!-- // footer -->
+        
+	</div>
+	<!-- // container -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php require_once('includes/head_section.php') ?>
+	<title>LifeBlog | Home </title>
+</head>
+<body>
+	<!-- container - wraps whole page -->
+	<div class="container">
+		<!-- navbar -->
+<?php include('includes/navbar.php') ?>
+		<!-- // navbar -->
+
+		<!-- Page content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</div>
+		<!-- // Page content -->
+
+		<!-- footer -->
+		<!-- footer -->
+<?php include('includes/footer.php') ?>
